e40e26a80f2f2bea90eb4c3a11294224de4921fb
Terence Buchannan <terencebuchannan@gmail.com>
2023-03-08 23:22:51 -0500

    feat(valid-anagrams): add solution

---
+++ easy/valid-anagrams.js
@@ -0,0 +1,53 @@
+/* 
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Execution: isAnagram("anagram", "nagaram")
+Output: true
+
+Execution: isAnagram("rat", "car")
+Output: false
+
+Time Complexity: O(N)
+Space Complexity: O(N)
+*/
+
+const letterCounter = (str) => {
+  let count = {}
+  for(let i =0; i < str.length; i++){
+    let char = str[i]
+    count[char] ? count[char] += 1 : count[char] = 1
+  }
+  return count
+}
+
+
+
+const isAnagram = (str1, str2) => {
+  if(str1.length !== str2.length) return false
+  let stringCount = letterCounter(str1);
+
+  for (let i = 0; i < str2.length; i++) {
+    let char = str2[i];
+    if(stringCount[char]) {
+      stringCount[char] -= 1
+    } else {
+      return false
+    }
+  }
+
+
+  for(let key in stringCount){
+    let val = stringCount[key]
+    if(val !== 0) return false
+  }
+
+  return true
+};
+
+console.log(isAnagram("anagram", "nagaram")); // -> true
+console.log(isAnagram("rat", "car")); // -> false
+
+
+
