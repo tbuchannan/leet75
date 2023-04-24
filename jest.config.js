93764e39728b9b61c675e9a4dcd935e2c1f2b918
Terence Buchannan <terencebuchannan@gmail.com>
2023-04-24 03:06:23 -0400

    chore(tests): stub in testing

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+  
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+  
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+  
+  // An array of regexp pattern strings used to skip coverage collection
+  coveragePathIgnorePatterns: [
+    "\\\\node_modules\\\\"
+  ],
+  
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+  
+  // A list of paths to directories that Jest should use to search for files in
+  roots: [
+    "<rootDir>"
+  ],
+  
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ]
+};
